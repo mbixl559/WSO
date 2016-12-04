ea4ffc679d9e3997324337dd12af7b955b030d47
Evan <etyrol3@gmail.com>
2016-12-03 23:38:07 -0500

    Fixed Parantheses Error

--- WSO/model.cs
+++ WSO/model.cs
@@ -28,21 +28,22 @@ namespace WSO {
 
         /// Query the database for a service with a date matching <datetime> and return the service
         /// If there is no such service return null
-        public Service getService(DateTime datetime) {
-            Service service;
-            using (var db = new WSOApp()) {
-                var tempServiceQuery = from s in db.Services
-                                       where s.Svc_DateTime == datetime
-                                       select s;
-                List<Service> results = tempServiceQuery.ToList();
-                if(results.Count > 0) {
-                    service = results[0];
-                } else {
-                    service = null;
+            public Service getService(DateTime datetime) {
+                Service service;
+                using (var db = new WSOApp()) {
+                    var tempServiceQuery = from s in db.Services
+                                           where s.Svc_DateTime == datetime
+                                           select s;
+                    List<Service> results = tempServiceQuery.ToList();
+                    if (results.Count > 0) {
+                        service = results[0];
+                    } else {
+                        service = null;
+                    }
                 }
-            }
 
-            return service;
+                return service;
+            }
         }
     }
 }
