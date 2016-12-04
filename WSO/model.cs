17a799ebea8422e8354e77822a85da5153b24e21
Mark Bixler <MarkBixler@ymail.com>
2016-12-03 22:51:00 -0500

    added a model class to do the non gui work

---
+++ WSO/model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSO
+{
+    class Model
+    {
+        public void getTemplateService(DateTime datetime) {
+
+            //add code to get the template service from the database
+
+            return;
+        }
+    }
+}
