83e4f46f5bad59756b142fb20ebc8fdba6e2a1c0
Mark Bixler <MarkBixler@ymail.com>
2016-12-03 23:10:11 -0500

    added a method to get the template service from the database

--- WSO/model.cs
+++ WSO/model.cs
@@ -7,13 +7,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WSO
-{
-    class Model
-    {
+namespace WSO { 
+
+    class WSOApp: DbContext {
+        public virtual DbSet<Service> Services { get; set; }
+    }
+
+    class Model {
         public void getTemplateService(DateTime datetime) {
 
             //add code to get the template service from the database
+            Service template;
+            using (var db = new WSOApp()) {
+                var tempServiceQuery = from s in db.Services
+                                       where s.Svc_DateTime == datetime
+                                       select s;
+                List<Service> results = tempServiceQuery.ToList();
+            }
 
             return;
         }
