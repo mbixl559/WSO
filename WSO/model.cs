eeae5df9de4bf907c863044fd53737d85007972a
Evan <etyrol3@gmail.com>
2016-12-03 23:31:20 -0500

    Added Tables to DbContext

--- WSO/model.cs
+++ WSO/model.cs
@@ -7,25 +7,37 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WSO { 
+namespace WSO {
 
-    class WSOApp: DbContext {
+    class WSOApp : DbContext
+    {
         public virtual DbSet<Service> Services { get; set; }
-    }
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<Ensemble> Ensembles { get; set; }
+        public virtual DbSet<EnsemblePerson> EnsemblePersons { get; set; }
+        public virtual DbSet<EventType> EventTypes { get; set; }
+        public virtual DbSet<PersonUnavailable> PersonUnavailables { get; set; }
+        public virtual DbSet<ServiceEvent> ServiceEvents { get; set; }
+        public virtual DbSet<Song> Songs { get; set; }
 
-    class Model {
-        public void getTemplateService(DateTime datetime) {
 
-            //add code to get the template service from the database
-            Service template;
-            using (var db = new WSOApp()) {
-                var tempServiceQuery = from s in db.Services
-                                       where s.Svc_DateTime == datetime
-                                       select s;
-                List<Service> results = tempServiceQuery.ToList();
+        class Model
+        {
+            public void getTemplateService(DateTime datetime)
+            {
+
+                //add code to get the template service from the database
+                Service template;
+                using (var db = new WSOApp())
+                {
+                    var tempServiceQuery = from s in db.Services
+                                           where s.Svc_DateTime == datetime
+                                           select s;
+                    List<Service> results = tempServiceQuery.ToList();
+                }
+
+                return;
             }
-
-            return;
         }
     }
 }
